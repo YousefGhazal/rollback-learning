ecd90a7a0aa79e9487eb31d0b054690fa876424f
Yousef Ghazal <yousefghazal2412@gmail.com>
2025-04-14 04:46:50 +0200

    first step

---
+++ python/dice_rolling_game.py
@@ -0,0 +1,23 @@
+import random
+
+number = input("how many time do you want to roll the dice? ")
+count = 0
+try:
+    number = int(number)
+    while number != 0: 
+        number -= 1
+        roll = input("roll the dice? (y/n): ").lower()
+        if roll == "y" :
+            count += 1
+            dice1 = random.randint(1, 6)
+            dice2 = random.randint(1, 6)
+            print (f"{dice1} , {dice2}")
+        elif roll == "n":
+            print ("bye bye")
+            break
+        else: 
+            print ("invalid input")
+    print (f"you rolled the dice {count} times")
+except ValueError:
+    print ("invalid input")
+         
